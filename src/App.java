e0abfe9482e72e6d971e150f0305bc65a2ac44c2
Felipe Trindade <fe_trindade01@hotmail.com>
2025-01-19 16:50:54 -0300

    Teste

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] lista = {1, 2, 3, 4, 5};
+
+        for (int i : lista) {
+            System.out.println(i);
+        }
+    }
+}
