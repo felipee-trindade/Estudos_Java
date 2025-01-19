5b2cb427e4e55837d08e0de0dd555b46d2743cc7
Felipe Trindade <fe_trindade01@hotmail.com>
2025-01-19 20:17:14 -0300

    conta nova

--- src/App.java
+++ src/App.java
@@ -2,8 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         int[] lista = {1, 2, 3, 4, 5};
 
-        for (int i : lista) {
-            System.out.println(i);
-        }
+        System.out.println("Olá mundo!");
     }
 }
