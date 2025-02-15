a787884269d7634a289d071b6969821e95f810a4
Felipe Trindade <fe_trindade01@hotmail.com>
2025-02-14 23:03:55 -0300

    Ex_003_2

--- src/Exercicios/Ex003.java
+++ src/Exercicios/Ex003.java
@@ -12,17 +12,23 @@ public class Ex003 {
         int[] numeros = new int[tamanho];
         System.out.print("Digite o valor de busca: ");
         int escolha = tc.nextInt();
+        int[] qtd_total = new int[101];
 
-        preencher(numeros, rand);
+
+        preencher(numeros, rand, qtd_total);
         buscar(numeros, escolha);
+        quantificar(numeros, qtd_total);
 
         tc.close();
         
     }
 
-    public static void preencher(int[] numeros, Random rand) {
+    public static void preencher(int[] numeros, Random rand, int[] qtd_total) {
         for(int i = 0; i < numeros.length; i++) {
-            numeros[i] = rand.nextInt(100);
+            numeros[i] = rand.nextInt(101);
+        }
+        for(int j = 0; j < qtd_total.length; j++) {
+            qtd_total[j] = 0;
         }
     }
 
@@ -30,7 +36,7 @@ public class Ex003 {
         int qtd = 0;
         for (int i : numeros) {
             if(escolha == i) {
-                qtd++;
+                qtd++; 
             }
         }
         if(qtd > 0){
@@ -38,6 +44,15 @@ public class Ex003 {
         }
         else {
             System.out.printf("A quantidade do número %d no array é %d%n%n", escolha, qtd);
+        }
+    }
+
+    public static void quantificar(int[] numeros, int[] qtd_total) {
+        for (int i : numeros) {
+            qtd_total[i] += 1;
+        }
+        for (int j = 0; j < qtd_total.length; j++) {
+            System.out.printf("A quantidade do número [%03d] é: %6d%n", j, qtd_total[j]);
         }
     }
 }
